51d630dbdf561bcaadcea173dc248daa428202e1
Navid Hosseini <navid045@gmail.com>
2021-10-13 17:27:21 +0330

    first petshop sart contract

---
+++ contracts/Adoption.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Adoption {
+
+  address[16] public adopters;
+
+
+  constructor() public {
+  }
+
+
+  function adopt(uint petId) public returns(uint) {
+    require(petId >= 0 && petId <= 15);
+    adopters[petId] = msg.sender;
+    return petId;
+  }
+
+  function getAdopters() public view returns(address[16] memory) {
+    return adopters;
+  }
+}
